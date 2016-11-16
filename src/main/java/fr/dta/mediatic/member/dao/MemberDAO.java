081fd6f0a1bf01905933e30ea9a5cb1c30b5735a
Nadir <nadir.allam@gmail.com>
2016-11-16 16:56:57 +0100

    Ajout de la méthode findMembersFromMedia

--- src/main/java/fr/dta/mediatic/member/dao/MemberDAO.java
+++ src/main/java/fr/dta/mediatic/member/dao/MemberDAO.java
@@ -1,6 +1,13 @@
 package fr.dta.mediatic.member.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import fr.dta.mediatic.helper.DataBaseHelper;
 import fr.dta.mediatic.helper.GenericDAO;
+import fr.dta.mediatic.media.model.Media;
 import fr.dta.mediatic.member.model.Member;
 
 public class MemberDAO extends GenericDAO<Member> {
@@ -26,19 +33,31 @@ public class MemberDAO extends GenericDAO<Member> {
 		return dao;
 	}
 	
+	/*
 	public static void addMember(Member member) {
-		
 		dao.persist(member);
 	}
 	
 	public static void removeMember(Member member) {
-		
 		dao.remove(member.getIdentifier());
 	}
 	
 	public static void modifyMenber(Member member) {
-		
 		dao.merge(member);
+	}
+	*/
+	
+	public List<Member> findMembersFromMedia(Media media) {
+		EntityManager em = DataBaseHelper.createEntityManager();
+		DataBaseHelper.beginTx(em);
+		TypedQuery<Member> query = em.createQuery("SELECT m "
+											   + "FROM Member m "
+											   + "JOIN m.listLoan l "
+											   + "WHERE l.media = :id ", Member.class);
+		query.setParameter("id", media.getId());
+		List<Member> listeReturn = query.getResultList();
+		em.close();
+		return listeReturn;
 	}
 	
 }
