b64b04112e41347dc643ea02654f759797e9257f
Baptiste Train <baptiste.train@gmail.com>
2016-11-16 15:01:49 +0100

    Ajout de la classe User

---
+++ src/main/java/fr/dta/mediatic/user/model/User.java
@@ -0,0 +1,88 @@
+package fr.dta.mediatic.user.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import fr.dta.mediatic.model.Gender;
+import fr.dta.mediatic.model.Person;
+
+@Entity
+@Table(name="user")
+public class User {
+	
+	/* COLUMN */
+	
+	@Id
+	@Column(name="login")
+	private String login;
+	
+	@NotBlank
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="role")
+	private Role role;
+	
+	@Embedded
+	private Person person;
+
+	public User() {
+
+	}
+
+	public User(String login, String password, Role role, String lastname, String firstname, String email, Gender gender) {
+
+		this.login = login;
+		this.password = password;
+		this.role = role;
+		this.person = new Person(lastname, firstname, email, gender);
+	}
+
+	public String getLogin() {
+		
+		return login;
+	}
+
+	public void setLogin(String login) {
+		
+		this.login = login;
+	}
+
+	public String getPassword() {
+		
+		return password;
+	}
+
+	public void setPassword(String password) {
+		
+		this.password = password;
+	}
+
+	public Role getRole() {
+		
+		return role;
+	}
+
+	public void setRole(Role role) {
+		
+		this.role = role;
+	}
+
+	public Person getPerson() {
+		
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		
+		this.person = person;
+	}
+	
+	
+
+}
