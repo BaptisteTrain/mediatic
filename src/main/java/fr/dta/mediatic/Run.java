4d058f2fc28f1e9dc0a03fd242e9e11b6a5ce736
Fanny Couturier <fanny.couturier.pro@gmail.com>
2016-11-29 17:46:03 +0100

    modif id member

--- src/main/java/fr/dta/mediatic/Run.java
+++ src/main/java/fr/dta/mediatic/Run.java
@@ -132,29 +132,29 @@ public class Run {
 
 
 	// Membres
-	Member mm1 = memberDAO.persist(new Member(null, new Date(), "Troun", "Bariste", "baba@gmail.com", Gender.Homme, "3 rue Apre", "01000", "AINVILLE", sub1));
-	Member mm2 = memberDAO.persist(new Member(null, new Date(), "Blanc", "Fanchon", "baba@gmail.com", Gender.Homme, "40 rue des Lacs", "78800", "HOUILLES", sub2));
-	Member mm3 = memberDAO.persist(new Member(null, new Date(), "Beaujolie", "Guy", "baba@gmail.com", Gender.Homme, "38 boulevard Bryas", "60100", "CREIL", sub3));
-	Member mm4 = memberDAO.persist(new Member(null, new Date(), "Pépin", "Talbot", "baba@gmail.com", Gender.Homme, "59 rue des Chaligny", "06200", "NICE", sub4));
-	Member mm5 = memberDAO.persist(new Member(null, new Date(), "LaGarde", "Heloise", "baba@gmail.com", Gender.Femme, "41 rue Léon Dierx", "59160", "LOMME", sub5));
-	Member mm6 = memberDAO.persist(new Member(null, new Date(), "Morel", "Loyal", "baba@gmail.com", Gender.Homme, "35 rue Lenotre", "35700", "RENNES", sub6));
-	Member mm7 = memberDAO.persist(new Member(null, new Date(), "Moreau", "Jacques", "baba@gmail.com", Gender.Homme, "60 Faubourg Saint Honoré", "75018", "PARIS", sub7));
-	Member mm8 = memberDAO.persist(new Member(null, new Date(), "Champagne", "Lotye", "baba@gmail.com", Gender.Homme, "47 rue Gontier-Patin", "80100", "ABBEVILLE", sub8));
-	Member mm9 = memberDAO.persist(new Member(null, new Date(), "Guernon", "Dexter", "baba@gmail.com", Gender.Homme, "45 Avenue des Pr'es", "78180", "MONTIGNY-LE-BRETONNEUX", sub9));
-	Member mm10 = memberDAO.persist(new Member(null, new Date(), "Pitre", "Moore", "baba@gmail.com", Gender.Homme, "67 Rue Marie De Médicis", "06400", "CANNES", sub10));
-	Member mm11 = memberDAO.persist(new Member(null, new Date(), "LaGarde", "Garland", "baba@gmail.com", Gender.Homme, "47 avenue de Provence", "56000", "VANNES", sub11));
-	Member mm12 = memberDAO.persist(new Member(null, new Date(), "Plante", "Royden", "baba@gmail.com", Gender.Homme, "7 avenue Jules Ferry", "02200", "SOISSONS", sub12));
-	Member mm13 = memberDAO.persist(new Member(null, new Date(), "Therrien", "Denise", "baba@gmail.com", Gender.Femme, "98 Chemin Des Bateliers", "74000", "ANNECY", sub13));
-	Member mm14 = memberDAO.persist(new Member(null, new Date(), "Brochu", "Odo", "baba@gmail.com", Gender.Homme, "67 rue Sadi Carnot", "93600", "AULNAY-SOUS-BOIS", sub14));
-	Member mm15 = memberDAO.persist(new Member(null, new Date(), "Gervais", "Blondelle", "baba@gmail.com", Gender.Femme, "73 quai Saint-Nicolas", "37100", "TOURS", sub15));
-	Member mm16 = memberDAO.persist(new Member(null, new Date(), "Nadeau", "Blondelle", "baba@gmail.com", Gender.Femme, "13 place de Miremont", "47300", "VILLENEUVE-SUR-LOT", sub16));
-	Member mm17 = memberDAO.persist(new Member(null, new Date(), "Ricard", "Josette", "baba@gmail.com", Gender.Femme, "22 rue des Lacs", "62110", "HÉNIN-BEAUMONT", sub17));
-	Member mm18 = memberDAO.persist(new Member(null, new Date(), "Faure", "Campbell", "baba@gmail.com", Gender.Homme, "91 rue Sébastopol", "97230", "SAINTE-MARIE", sub18));
-	Member mm19 = memberDAO.persist(new Member(null, new Date(), "Étoile", "Xavier", "baba@gmail.com", Gender.Homme, "7 rue du Faubourg National", "94320", "THIAIS", sub19));
-	Member mm20 = memberDAO.persist(new Member(null, new Date(), "Rochefort", "Eliot", "baba@gmail.com", Gender.Homme, "48 rue de Raymond Poincaré", "11100", "NARBONNE", sub20));
-	Member mm21 = memberDAO.persist(new Member(null, new Date(), "Clavet", "Evrard", "baba@gmail.com", Gender.Homme, "18 Faubourg Saint Honoré", "75018", "PARIS", sub21));
-	Member mm22 = memberDAO.persist(new Member(null, new Date(), "Bureau", "Denis", "baba@gmail.com", Gender.Homme, "11 boulevard de la Liberation", "13013", "MARSEILLE", sub22));
-	Member mm23 = memberDAO.persist(new Member(null, new Date(), "Marquis", "Brice", "baba@gmail.com", Gender.Homme, "49 rue Descartes", "67200", "STRASBOURG", sub23));
+	Member mm1 = memberDAO.persist(new Member(null, "IDFG01", new Date(), "Troun", "Bariste", "baba@gmail.com", Gender.Homme, "3 rue Apre", "01000", "AINVILLE", sub1));
+	Member mm2 = memberDAO.persist(new Member(null, "IDFG02", new Date(), "Blanc", "Fanchon", "baba@gmail.com", Gender.Homme, "40 rue des Lacs", "78800", "HOUILLES", sub2));
+	Member mm3 = memberDAO.persist(new Member(null, "IDFG03", new Date(), "Beaujolie", "Guy", "baba@gmail.com", Gender.Homme, "38 boulevard Bryas", "60100", "CREIL", sub3));
+	Member mm4 = memberDAO.persist(new Member(null, "IDFG04", new Date(), "Pépin", "Talbot", "baba@gmail.com", Gender.Homme, "59 rue des Chaligny", "06200", "NICE", sub4));
+	Member mm5 = memberDAO.persist(new Member(null, "IDFG05", new Date(), "LaGarde", "Heloise", "baba@gmail.com", Gender.Femme, "41 rue Léon Dierx", "59160", "LOMME", sub5));
+	Member mm6 = memberDAO.persist(new Member(null, "IDFG06", new Date(), "Morel", "Loyal", "baba@gmail.com", Gender.Homme, "35 rue Lenotre", "35700", "RENNES", sub6));
+	Member mm7 = memberDAO.persist(new Member(null, "IDFG07", new Date(), "Moreau", "Jacques", "baba@gmail.com", Gender.Homme, "60 Faubourg Saint Honoré", "75018", "PARIS", sub7));
+	Member mm8 = memberDAO.persist(new Member(null, "IDFG08", new Date(), "Champagne", "Lotye", "baba@gmail.com", Gender.Homme, "47 rue Gontier-Patin", "80100", "ABBEVILLE", sub8));
+	Member mm9 = memberDAO.persist(new Member(null, "IDFG09", new Date(), "Guernon", "Dexter", "baba@gmail.com", Gender.Homme, "45 Avenue des Pr'es", "78180", "MONTIGNY-LE-BRETONNEUX", sub9));
+	Member mm10 = memberDAO.persist(new Member(null, "IDFG10", new Date(), "Pitre", "Moore", "baba@gmail.com", Gender.Homme, "67 Rue Marie De Médicis", "06400", "CANNES", sub10));
+	Member mm11 = memberDAO.persist(new Member(null, "IDFG11", new Date(), "LaGarde", "Garland", "baba@gmail.com", Gender.Homme, "47 avenue de Provence", "56000", "VANNES", sub11));
+	Member mm12 = memberDAO.persist(new Member(null, "IDFG12", new Date(), "Plante", "Royden", "baba@gmail.com", Gender.Homme, "7 avenue Jules Ferry", "02200", "SOISSONS", sub12));
+	Member mm13 = memberDAO.persist(new Member(null, "IDFG13", new Date(), "Therrien", "Denise", "baba@gmail.com", Gender.Femme, "98 Chemin Des Bateliers", "74000", "ANNECY", sub13));
+	Member mm14 = memberDAO.persist(new Member(null, "IDFG14", new Date(), "Brochu", "Odo", "baba@gmail.com", Gender.Homme, "67 rue Sadi Carnot", "93600", "AULNAY-SOUS-BOIS", sub14));
+	Member mm15 = memberDAO.persist(new Member(null, "IDFG15", new Date(), "Gervais", "Blondelle", "baba@gmail.com", Gender.Femme, "73 quai Saint-Nicolas", "37100", "TOURS", sub15));
+	Member mm16 = memberDAO.persist(new Member(null, "IDFG16", new Date(), "Nadeau", "Blondelle", "baba@gmail.com", Gender.Femme, "13 place de Miremont", "47300", "VILLENEUVE-SUR-LOT", sub16));
+	Member mm17 = memberDAO.persist(new Member(null, "IDFG17", new Date(), "Ricard", "Josette", "baba@gmail.com", Gender.Femme, "22 rue des Lacs", "62110", "HÉNIN-BEAUMONT", sub17));
+	Member mm18 = memberDAO.persist(new Member(null, "IDFG18", new Date(), "Faure", "Campbell", "baba@gmail.com", Gender.Homme, "91 rue Sébastopol", "97230", "SAINTE-MARIE", sub18));
+	Member mm19 = memberDAO.persist(new Member(null, "IDFG19", new Date(), "Étoile", "Xavier", "baba@gmail.com", Gender.Homme, "7 rue du Faubourg National", "94320", "THIAIS", sub19));
+	Member mm20 = memberDAO.persist(new Member(null, "IDFG20", new Date(), "Rochefort", "Eliot", "baba@gmail.com", Gender.Homme, "48 rue de Raymond Poincaré", "11100", "NARBONNE", sub20));
+	Member mm21 = memberDAO.persist(new Member(null, "IDFG21", new Date(), "Clavet", "Evrard", "baba@gmail.com", Gender.Homme, "18 Faubourg Saint Honoré", "75018", "PARIS", sub21));
+	Member mm22 = memberDAO.persist(new Member(null, "IDFG22", new Date(), "Bureau", "Denis", "baba@gmail.com", Gender.Homme, "11 boulevard de la Liberation", "13013", "MARSEILLE", sub22));
+	Member mm23 = memberDAO.persist(new Member(null, "IDFG23", new Date(), "Marquis", "Brice", "baba@gmail.com", Gender.Homme, "49 rue Descartes", "67200", "STRASBOURG", sub23));
 
 	// Emprunt
 	loadDAO.persist(new Loan(null, new Date(), new Date(), new Date(), md1, mm1));
@@ -251,9 +251,9 @@ public class Run {
 	System.out.println("--------NEW MEMBER---------");
 	Subscription sub1 = subscriptionDAO.persist(new Subscription(300, new Date()));
 	Subscription sub2 = subscriptionDAO.persist(new Subscription(200, new Date()));
-	Member member1 = new Member(null, new Date(), "Dupont", "Jacques", "jacques@gmail.com", Gender.Homme, "3 rue Apre", "01000", "AINVILLE", sub1);
+	Member member1 = new Member(null, "IDFG01", new Date(), "Dupont", "Jacques", "jacques@gmail.com", Gender.Homme, "3 rue Apre", "01000", "AINVILLE", sub1);
 	memberDAO.persist(member1);
-	Member member2 = new Member(null, new Date(), "Dupond", "Jacquart", "jacques2@gmail.com", Gender.Homme, "3 rue Apres", "01000", "AINVILLE", sub2);
+	Member member2 = new Member(null, "IDFG45", new Date(), "Dupond", "Jacquart", "jacques2@gmail.com", Gender.Homme, "3 rue Apres", "01000", "AINVILLE", sub2);
 	memberDAO.persist(member2);
 	System.out.println("--------/NEW MEMBER---------");
 
@@ -265,7 +265,7 @@ public class Run {
 
 	// Remove member
 	System.out.println("--------REMOVE MEMBER---------");
-	memberDAO.remove(member1.getIdentifier());
+	memberDAO.remove(member1.getId());
 	System.out.println("--------/REMOVE MEMBER---------");
 
 	List<Member> listM;
@@ -280,17 +280,17 @@ public class Run {
 
 	// Select member by id
 	System.out.println("--------MEMBER BY ID---------");
-	Member memberFind = memberDAO.find(member2.getIdentifier());
+	Member memberFind = memberDAO.find(member2.getId());
 	System.out.println(">>>>>>>>>>>>MEMBER = " + memberFind.getPerson().getFirstname());
 	System.out.println("--------/MEMBER BY ID---------");
 	
 	// Select member by id partiel
-	/*System.out.println("--------MEMBER BY ID PARTIAL---------");
-	listM = memberDAO.findMembersByIdPartial(member1.getIdentifier().intValue());
+	System.out.println("--------MEMBER BY ID PARTIAL---------");
+	listM = memberDAO.findMembersByIdPartial(member1.getIdentifier());
 	for (Member m : listM) {
 	    System.out.println(">>>>>>>>>>>MEMBER = " + m);
 	    System.out.println("--------/MEMBER BY ID PARTIAL--------");
-	}*/
+	}
 
 	// Select member by name
 	System.out.println("--------MEMBER BY NAME---------");
