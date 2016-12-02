c9753ae83551b257984a8cc0b783f88523795431
seriux55 <seriux55@hotmail.com>
2016-12-02 06:11:36 +0100

    Mise en forme

--- src/main/java/fr/dta/mediatic/loan/model/Loan.java
+++ src/main/java/fr/dta/mediatic/loan/model/Loan.java
@@ -11,89 +11,89 @@ import fr.dta.mediatic.member.model.Member;
 @Table(name = "loan")
 public class Loan {
 
-    @Id
-    @GeneratedValue
-    private Long id;
+	@Id
+	@GeneratedValue
+	private Long id;
 
-    @Temporal(TemporalType.DATE)
-    private Date loanDate;
+	@Temporal(TemporalType.DATE)
+	private Date loanDate;
 
-    @Temporal(TemporalType.DATE)
-    private Date returnDate;
+	@Temporal(TemporalType.DATE)
+	private Date returnDate;
 
-    @Temporal(TemporalType.DATE)
-    private Date plannedReturnDate;
+	@Temporal(TemporalType.DATE)
+	private Date plannedReturnDate;
 
-    @ManyToOne
-    private Media media;
+	@ManyToOne
+	private Media media;
 
-    @ManyToOne
-    private Member member;
+	@ManyToOne
+	private Member member;
 
-    public Loan() {
-    }
+	public Loan() {
+	}
 
-    public Loan(Long id, Date loanDate, Date returnDate, Date plannedReturnDate, Media media, Member member) {
-	super();
-	this.id = id;
-	this.loanDate = loanDate;
-	this.returnDate = returnDate;
-	this.plannedReturnDate = plannedReturnDate;
-	this.media = media;
-	this.member = member;
-    }
+	public Loan(Long id, Date loanDate, Date returnDate, Date plannedReturnDate, Media media, Member member) {
+		super();
+		this.id = id;
+		this.loanDate = loanDate;
+		this.returnDate = returnDate;
+		this.plannedReturnDate = plannedReturnDate;
+		this.media = media;
+		this.member = member;
+	}
 
-    public Long getId() {
-	return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-	this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Date getLoanDate() {
-	return loanDate;
-    }
+	public Date getLoanDate() {
+		return loanDate;
+	}
 
-    public void setLoanDate(Date loanDate) {
-	this.loanDate = loanDate;
-    }
+	public void setLoanDate(Date loanDate) {
+		this.loanDate = loanDate;
+	}
 
-    public Date getReturnDate() {
-	return returnDate;
-    }
+	public Date getReturnDate() {
+		return returnDate;
+	}
 
-    public void setReturnDate(Date returnDate) {
-	this.returnDate = returnDate;
-    }
+	public void setReturnDate(Date returnDate) {
+		this.returnDate = returnDate;
+	}
 
-    public Date getPlannedReturnDate() {
-	return plannedReturnDate;
-    }
+	public Date getPlannedReturnDate() {
+		return plannedReturnDate;
+	}
 
-    public void setPlannedReturnDate(Date plannedReturnDate) {
-	this.plannedReturnDate = plannedReturnDate;
-    }
+	public void setPlannedReturnDate(Date plannedReturnDate) {
+		this.plannedReturnDate = plannedReturnDate;
+	}
 
-    public Media getMedia() {
-	return media;
-    }
+	public Media getMedia() {
+		return media;
+	}
 
-    public void setMedia(Media media) {
-	this.media = media;
-    }
+	public void setMedia(Media media) {
+		this.media = media;
+	}
 
-    public Member getMember() {
-	return member;
-    }
+	public Member getMember() {
+		return member;
+	}
 
-    public void setMember(Member member) {
-	this.member = member;
-    }
+	public void setMember(Member member) {
+		this.member = member;
+	}
 
-    @Override
-    public String toString() {
-	return "Loan [id=" + id + ", loanDate=" + loanDate + ", returnDate=" + returnDate + ", plannedReturnDate=" + plannedReturnDate + ", media=" + media + ", member=" + member + "]";
-    }
-
+	@Override
+	public String toString() {
+		return "Loan [id=" + id + ", loanDate=" + loanDate + ", returnDate=" + returnDate + ", plannedReturnDate="
+				+ plannedReturnDate + ", media=" + media + ", member=" + member + "]";
+	}
 }
