304c611e91bf31f2722a61db855af1e385029003
Matthieu Laclau <matthieu.laclau@gmail.com>
2016-12-02 15:33:57 +0100

    Membersheet avancée

--- ui/app/member/memberSheetCtrl.js
+++ ui/app/member/memberSheetCtrl.js
@@ -1,6 +1,46 @@
 angular.module('MemberSheet', [])
-	.controller('MemberSheetController', ['$scope', function($scope) {
+	.controller('MemberSheetController', ['$http', 'MemberSheetService', '$routeParams', function($http, MemberSheetService, $routeParams) {
 		var mbshCtrl = this;
+		console.log($routeParams.idMember);
+		
+		console.log("Ici");
+		MemberSheetService.getSheet($routeParams.idMember).then(function(liste) {
+			console.log("La fiche du membre est: ");
+			console.log(liste);
+			mbshCtrl.lastname = liste.nom;
+			mbshCtrl.firstname = liste.prenom;
+			var pattern = "/(\d{4})\-(\d{2})\-(\d{2})/";
+			mbshCtrl.birthdate = new Date(liste.date_naissance.replace(pattern,'$1-$2-$3'));
+			mbshCtrl.email = liste.email;
+			mbshCtrl.address = liste.adresse.ligne1 + " " + liste.adresse.ligne2;
+			mbshCtrl.postalcode =  liste.adresse.codepostal;
+			mbshCtrl.town =  liste.adresse.ville;
+			mbshCtrl.age = liste.age;
+			mbshCtrl.subscriptionAmount = liste.cotisation.montant;
+			mbshCtrl.subscriptionDate = new Date(liste.cotisation.debut.replace(pattern,'$1-$2-$3'));
+			
+
+			console.log("Emprunts: ");
+			console.log(liste);
+		});
+		
+		console.log("mbshCtrl.sheet: ");
+		console.log(mbshCtrl.sheet);
+		
+		/*for(var i in mbshCtrl.sheet) {
+			console.log(mbshCtrl.sheet[i]);
+			for(var j in mbshCtrl.sheet[i]) {
+				console.log(mbshCtrl.sheet[i][j]);
+			}
+		}*/
+		
+		angular.forEach(mbshCtrl.sheet, function(member) {
+			console.log("Member: ");
+			console.log(member);
+			angular.forEach(member, function(dd) {
+				console.log("dd: " + dd);
+			});
+		});
 		
 		mbshCtrl.loans =
 			[
@@ -26,14 +66,75 @@ angular.module('MemberSheet', [])
 				}
 			];
 		
-		mbshCtrl.books =
+		mbshCtrl.medias =
 			[
+				{
+					title: 'Java pour les Ploucs',
+					author: 'Nadir ALLAM',
+					reference: '978-2075074209',
+					type: 'book'
+				},
 				
+				{
+					title: 'L\'informatique est un long fleuve tranquille',
+					author: 'Fanny COUTURIER',
+					reference: 'B01K2WUR9E',
+					type: 'dvd'
+				},
+				
+				{
+					title: 'CSS Dance Party Vol. 5',
+					author: 'Matthieu LACLAU',
+					reference: 'B01GO3JWPK',
+					type: 'cd'
+				},
+				
+				{
+					title: 'Sopra, si près',
+					author: 'Baptiste TRAIN',
+					reference: '978-3155058290',
+					type: 'book'
+				},
+				
+				{
+					title: 'La mélancolie du Javascript',
+					author: 'Fanny COUTURIER',
+					reference: '978-5135666210',
+					type: 'book'
+				},
+				
+				{
+					title: 'La Bible de Jquery',
+					author: 'Matthieu LACLAU',
+					reference: '978-5554867824',
+					type: 'book'
+				},
+				
+				{
+					title: 'Sport et Informatique: Comment se muscler le cerveau',
+					author: 'Nadir ALLAM',
+					reference: 'B01L3XTC9E',
+					type: 'dvd'
+				}
 			];
 	}])
+	.factory('MemberSheetService', function($http) {
+		var url = 'http://192.168.10.34:8090/resource/adherent.accession?id=';
+		
+		return {
+			getSheet : function(id) {
+				console.log("ID: " + id);
+				console.log("URL+ID: " + url + id);
+				return $http.get(url + id).then(function(response) {
+					console.log("Data: " + response.data);
+					return response.data;
+				});
+			}
+		}
+	})
 	.config(function($routeProvider) {
-		$routeProvider.when('/member/sheet', {
-			templateUrl: '/member/fiche_adherent.html',
+		$routeProvider.when('/member/:idMember', {
+			templateUrl: '/member/memberSheet.html',
 			controller: 'MemberSheetController',
 			controllerAs: 'mbshCtrl'
 		});
