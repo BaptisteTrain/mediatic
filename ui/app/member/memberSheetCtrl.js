66c4c36f18c31ad509f590e7030581202ce99c93
Matthieu Laclau <matthieu.laclau@gmail.com>
2016-12-02 12:43:09 +0100

    MAJ Fiche adherent

--- ui/app/member/memberSheetCtrl.js
+++ ui/app/member/memberSheetCtrl.js
@@ -1,11 +1,37 @@
-angular.module('MemberSheetApp', [])
+angular.module('MemberSheet', [])
 	.controller('MemberSheetController', ['$scope', function($scope) {
-		var msCtrl = this;
+		var mbshCtrl = this;
+		
+		mbshCtrl.loans =
+			[
+				{
+					reference: '123456798',
+					type: 'cd',
+					title: 'Star Wars',
+					loandate: '02/11/2016'
+				},
+				
+				{
+					reference: '123456789',
+					type: 'book',
+					title: 'Le livre de la jungle',
+					loandate: '03/11/2016'
+				},
+				
+				{
+					reference: '543216798',
+					type: 'dvd',
+					title: 'Suits: avocats sur mesure',
+					loandate: '08/11/2016'
+				}
+			];
+		
+		mbshCtrl.books =
 	}])
 	.config(function($routeProvider) {
 		$routeProvider.when('/member', {
 			templateUrl: '/member/fiche_adherent.html',
 			controller: 'MemberSheetController',
-			controllerAs: 'msCtrl'
+			controllerAs: 'mbshCtrl'
 		});
 	})
