d9ebb595f018993c9bc996eccb0087046eac0c31
seriux55 <seriux55@hotmail.com>
2016-12-02 05:39:12 +0100

    Ajout de fonction le nombre de médias empruntés

--- src/main/java/fr/dta/mediatic/loan/dao/LoanDAO.java
+++ src/main/java/fr/dta/mediatic/loan/dao/LoanDAO.java
@@ -2,7 +2,12 @@ package fr.dta.mediatic.loan.dao;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import fr.dta.mediatic.helper.DataBaseHelper;
 import fr.dta.mediatic.helper.GenericDAO;
 import fr.dta.mediatic.loan.model.Loan;
 import fr.dta.mediatic.media.model.Media;
@@ -25,15 +30,35 @@ public class LoanDAO extends GenericDAO<Loan> {
 		return dao;
 	}
 
+	/**
+	 * Add a loan's media
+	 * @param member
+	 * @param media
+	 * @return
+	 */
 	public void insertLoan(Member member, Media media){
 		Loan loan = new Loan();
 		loan.setLoanDate(new Date());
 		loan.setReturnDate(null); 
-		Calendar today = Calendar.getInstance(); // To have the date of today
+		Calendar today = Calendar.getInstance(); // To have the today date
 		today.add(Calendar.DATE, TypeMedia.getDuration(media.getType())); // To have today date plus duration
 		loan.setPlannedReturnDate(today.getTime());
 		loan.setMedia(media);
 		loan.setMember(member);
 		persist(loan);
+	}
+
+	/**
+	 * Count how many loan's media
+	 * @return int
+	 */
+	public int howManyLoan() {
+		EntityManager em = DataBaseHelper.createEntityManager();
+		DataBaseHelper.beginTx(em);
+		TypedQuery<Loan> query = em.createQuery("SELECT count(l) "
+											   + "FROM Loan l ", Loan.class);
+		int count = query.getMaxResults();
+		em.close();
+		return count;
 	}
 }
