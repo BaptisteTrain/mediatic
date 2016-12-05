ca3d4cb7dcd0ba2bd05317222f759a9504b1615d
Baptiste Train <baptiste.train@gmail.com>
2016-12-05 16:44:29 +0100

    Mise à jour de la fiche de création d'adhérent.

--- ui/app/view-member/createMemberCtrl.js
+++ ui/app/view-member/createMemberCtrl.js
@@ -1,39 +1,44 @@
 angular.module('CreateMember', [])
-	.controller('CreateMemberCtrl', function() {
+	.controller('CreateMemberCtrl', ['$http', 'MemberSheetService',  function($http, MemberSheetService) {
 		var obj = this;
 		
-		createMemberCtrl.setMemberSheet = function() {
-			console.log("Lastname: " + createMemberCtrl.lastname);
-			console.log("Firstname: " + createMemberCtrl.firstname);
-			console.log("Birthdate: " + createMemberCtrl.birthdate);
-			console.log("E-mail: " + createMemberCtrl.email);
-			console.log("Address: " + createMemberCtrl.ligne1);
-			console.log("Address: " + createMemberCtrl.ligne2);
-			console.log("Postal Code: " + createMemberCtrl.postalcode);
-			console.log("Town: " + createMemberCtrl.town);
-			console.log("Age: " + createMemberCtrl.age);
-			console.log("Subscription Amount: " + createMemberCtrl.subscriptionAmount);
-			console.log("Subscription Date: " + createMemberCtrl.subscriptionDate);
-			console.log("Loans: " + createMemberCtrl.loans);
+		obj.setMemberSheet = function() {
+			console.log("Lastname: " + obj.lastname);
+			console.log("Firstname: " + obj.firstname);
+			console.log("Birthdate: " + obj.birthdate);
+			console.log("E-mail: " + obj.email);
+			console.log("Address: " + obj.ligne1);
+			console.log("Address: " + obj.ligne2);
+			console.log("Postal Code: " + obj.postalcode);
+			console.log("Town: " + obj.town);
+			console.log("Age: " + obj.age);
+			console.log("Subscription Amount: " + obj.subscriptionAmount);
+			console.log("Subscription Date: " + obj.subscriptionDate);
+			console.log("Loans: " + obj.loans);
 			
 			var data = {
-                id: parseInt($routeParams.idMember),
-                nom: createMemberCtrl.lastname,
-                prenom: createMemberCtrl.firstname,
-                date_naissance: createMemberCtrl.birthdate,
-                email: createMemberCtrl.email,
-                adresse: {ligne1: createMemberCtrl.ligne1, ligne2: createMemberCtrl.ligne2, codepostal: createMemberCtrl.postalcode, ville: createMemberCtrl.town},
-                cotisation: {debut: createMemberCtrl.subscriptionDate, fin: createMemberCtrl.subscriptionEndDate, montant: createMemberCtrl.subscriptionAmount},
-                age: createMemberCtrl.age,
-                emprunt: createMemberCtrl.loans,
-                nombre_media: createMemberCtrl.nbLoans
+                nom: obj.lastname,
+                prenom: obj.firstname,
+                date_naissance: obj.birthdate,
+                email: obj.email,
+                adresse: {ligne1: obj.ligne1, ligne2: obj.ligne2, codepostal: obj.postalcode, ville: obj.town},
+                cotisation: {debut: obj.subscriptionDate, fin: obj.subscriptionEndDate, montant: obj.subscriptionAmount},
+                age: obj.age,
+                emprunt: obj.loans,
+                nombre_media: obj.nbLoans
             };
+			
+			MemberSheetService.setSheet(data).then(function(response) {
+				console.log("L'adhérent a été correctement enregistré");
+			}, function(reason) {
+				console.log("L'adhérent n'a pas pu être enregistré");
+			});
 			
 			console.log("Data à envoyer: ");
 			console.log(data);
 		}
 		
-	})
+	}])
 	.config(function($routeProvider) {
 		$routeProvider.when('/createMember', {
 			templateUrl : '/view-member/createMember.html',
