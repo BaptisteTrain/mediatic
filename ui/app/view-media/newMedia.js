0885bfa2c9a61e1519515762a3038ac75fc50659
Fanny Couturier <fanny.couturier.pro@gmail.com>
2016-12-05 11:45:35 +0100

    implementation angular newMedia

--- ui/app/view-media/newMedia.js
+++ ui/app/view-media/newMedia.js
@@ -15,12 +15,19 @@ angular.module('NewMedia', [])
 	var self = this;
 
 	// Titre de la page
-	$rootScope.titre = "Nouveau Media";
+	$rootScope.titre = 'Nouveau Media';
 	
 	// Redirection
 	this.goBackMedia = function() {
 		$location.url('/media');
 	}
+	
+	// Add a new media
+	this.addMedia = function() {
+		var url = 'http://192.168.10.34:8090/resource/media.creation';
+		var mediaToSend = {titre: self.mediaToAdd.title, auteur: self.mediaToAdd.author, type: self.mediaToAdd.type};
+		$http.post(url, mediaToSend).then(function(response) {});
+	};
 	
 	
 }]);
