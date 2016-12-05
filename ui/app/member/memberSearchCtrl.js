20ffe99e313c70921c29473215e6e356829921fa
Baptiste Train <baptiste.train@gmail.com>
2016-12-05 12:26:54 +0100

    Correcction de la pagination

--- ui/app/member/memberSearchCtrl.js
+++ ui/app/member/memberSearchCtrl.js
@@ -40,7 +40,13 @@ angular.module('MemberSearch', [])
 			if (input != undefined && input != null && input != '') {
 				
 				var start = (currentPage - 1) * itemsPerPage;
-				var end = start + itemsPerPage;
+				var end;
+				
+				if ((input.length - start) < itemsPerPage) {
+					end = start + (input.length - start);
+				} else {
+					end = start + itemsPerPage;
+				}
 
 				for (var i = start;i < end;i++) {
 					result.push(input[i]);
