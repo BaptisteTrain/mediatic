1e981d00fc8419d80abd2e6eba4deaadcb6d82b4
Fanny Couturier <fanny.couturier.pro@gmail.com>
2016-12-06 11:54:08 +0100

    service auth dans fichier js spec + sup lien mdp

--- ui/app/view-login/login.js
+++ ui/app/view-login/login.js
@@ -10,8 +10,8 @@ angular.module('Login', [])
 	});
 })
 
-.controller('LoginCtrl', ['$location', '$http', '$rootScope', 'AuthentificationService', 
-					function($location, $http, $rootScope, AuthentificationService) {
+.controller('LoginCtrl', ['$location', '$http', '$rootScope', 'AuthenticationService', 
+					function($location, $http, $rootScope, AuthenticationService) {
 	
 	var self = this;
 	
@@ -26,11 +26,11 @@ angular.module('Login', [])
 	$rootScope.memberActive = '';
 	
 	// Disconnect if previously was connected
-	AuthentificationService.disconnect();
+	AuthenticationService.disconnect();
 	
 	// Check the authentication
 	this.checkAuth = function() {
-		AuthentificationService.connect(this.id, this.psw).then(function(resolution) {
+		AuthenticationService.connect(this.id, this.psw).then(function(resolution) {
 			if (resolution) {
 				// Redirection
 				$location.url('/media');
@@ -41,74 +41,4 @@ angular.module('Login', [])
 		});
 	}
 
-}])
-
-
-.factory('AuthentificationService', function($http, IpService) {
-	// F5 bypass
-	//var defaut = 'Basic YXplOmF6ZQ==';
-	//$http.defaults.headers.common['Authorization'] = defaut;
-	
-	// If the user is connected
-	var connected = false;
-	
-	// Rights for the creation buttons
-	var rightMemberCreation = false;
-	var rightMediaCreation = false;
-	
-	return {
-		// Check the authentication
-		connect : function(login, password) {
-			var auth = 'Basic' + btoa(login + ':' + password);
-			var config = {
-				headers : {
-					'Authorization' : auth
-				}
-			};
-			var url = 'http://'+IpService+':8090/resource/connexion.rights';
-			
-			return $http.get(url, config).then(function(response) {
-				// Authentication OK
-				connected = true;
-				// Retrieve of the rights
-				for (var i in response.data) {
-					if (response.data[i] == 'creation-adherent') {
-						rightMemberCreation = true;
-					} else if (response.data[i] == 'creation-media') {
-						rightMediaCreation = true;
-					}
-				}
-				// Authentication for the http services
-				$http.defaults.headers.common['Authorization'] = auth;
-				return true;
-			}, function() {
-				// Authentication NOP
-				connected = false;
-				$http.defaults.headers.common['Authorization'] = 'Basic';
-				return false;
-			});
-		},
-		
-		// Tells if the user is authenticated
-		isConnected : function() {
-			return connected;
-		},
-		
-		// Disconnect the user
-		disconnect: function() {
-			connected = false;
-		},
-		
-		// Has the right to create a new member
-		hasRightMemberCreation: function() {
-			return rightMemberCreation;
-		},
-		
-		// Has the right to create a new member
-		hasRightMediaCreation: function() {
-			return rightMediaCreation;
-		}
-	}
-});
-
-;
+}]);
